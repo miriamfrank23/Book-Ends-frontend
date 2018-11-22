ba45e59d450ca360f771209a9fb82c41da57368e
miriamfrank23 <miriamfrank23@gmail.com>
2018-11-22 01:36:10 -0500

    updated css

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,12 +1,16 @@
 import React from 'react'
 import SearchBar from './SearchBar'
 
+// {*<img src='https://vignette.wikia.nocookie.net/harrypotter/images/d/d4/LibraryPottermore.png/revision/latest?cb=20160530005252' className="App-logo" alt="logo" />
+// *}
 const NavBar = ({ captureInput, currentBookId, noBookSelected }) => {
 
 
     return (
       <div className='navBar'>
-        <img src='https://vignette.wikia.nocookie.net/harrypotter/images/d/d4/LibraryPottermore.png/revision/latest?cb=20160530005252' className="App-logo" alt="logo" />
+      <h1 id='logo'>
+        Books
+      </h1>
         {currentBookId ?
           <button id='navBarButton'
           onClick={noBookSelected}>
@@ -18,7 +22,7 @@ const NavBar = ({ captureInput, currentBookId, noBookSelected }) => {
         }
         <div className='profileIcon'>
           <img src={'https://avatars2.githubusercontent.com/u/16786985?s=460&v=4'}
-          className='profileImage'/>
+          className='profileImage' alt=''/>
           View my profile
         </div>
       </div>
